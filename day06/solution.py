ed0d3499acf45e011910f978c385d0cb3b635de5
Wessel Radstok <wessel.radstok@nslogin.nl>
2023-12-06 07:25:22 +0100

    Solve day 6

---
+++ day06/solution.py
@@ -0,0 +1,25 @@
+import re
+import math
+
+with open("day06/input.txt") as f:
+    lines = f.read().splitlines()
+    times = re.findall(r"\d+", lines[0])
+    dists = re.findall(r"\d+", lines[1])
+
+# Part 1
+wins = []
+for time, dist in zip(map(int, times), map(int, dists)):
+    win = [speed for speed in range(time) if speed * (time - speed) > dist]
+    wins.append(len(win))
+
+print(math.prod(wins))
+
+# Part 2
+total_time = int("".join(times))
+total_dist = int("".join(dists))
+
+wins = 0
+for speed in range(total_time):
+    if speed * (total_time - speed) > total_dist:
+        wins += 1
+print(wins)
