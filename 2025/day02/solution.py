6282f1dcb8c64c951c5b1e4fbea2d11830a7fee0
Wessel Radstok <wessel.radstok@nslogin.nl>
2025-12-02 07:07:44 +0100

    Add day 2

--- 2025/day02/solution.py
+++ 2025/day02/solution.py
@@ -1,2 +1,33 @@
 with open("2025/day02/input.txt", "r") as f:
-    lines = f.read().splitlines()
+    line = f.read().splitlines()[0].split(",")
+
+ranges = []
+for section in line:
+    ranges.append(tuple(map(int, section.split("-"))))
+
+
+def is_repeating(n: str) -> bool:
+    return n[0 : len(n) // 2] == n[len(n) // 2 :]
+
+
+def is_mult_repeating(n: str) -> bool:
+    substrs = []
+    for i in range(0, len(n) // 2 + 1):
+        substrs.append(n[0:i])
+
+    for substr in substrs:
+        if n.count(substr) * len(substr) == len(n):
+            return True
+    return False
+
+
+invalid = 0
+for start, end in ranges:
+    for number in range(start, end + 1):
+        n = str(number)
+        # if is_repeating(n):
+        if is_mult_repeating(n):
+            invalid += number
+
+
+print(invalid)
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Advent of code
-Advent of code solutions in Python.
+Advent of code solutions in Python. I don't use any third party libraries.
 
 Each years puzzle was made in the year of release. I added them to this repo later. I only managed
 to make it to the end once, in 2023 :).
