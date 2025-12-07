f2d6677e9861d2c642e3257e89b6583ceb6829fb
Wessel Radstok <wessel.radstok@nslogin.nl>
2025-12-07 12:27:55 +0100

    Day 07

--- 2025/day07/solution.py
+++ 2025/day07/solution.py
@@ -1,46 +1,74 @@
-from math import prod
 from collections import defaultdict
-import re
+from functools import cache
 
 with open("2025/day07/input.txt") as f:
     lines = f.read().splitlines()
 
-problems = defaultdict(list)
-results = []
-for line in lines:
-    if "*" in line or "+" in line:
-        for i, op in enumerate(line.split()):
-            if op == "+":
-                results.append(sum(problems[i]))
-            if op == "*":
-                results.append(prod(problems[i]))
-    else:
-        nums = map(int, line.split())
-        for i, num in enumerate(nums):
-            problems[i].append(num)
+splitters: set[tuple[int, int]] = set()
+beam_starts = set()
+all_beams = set()
 
-print(sum(results))
+for new_y, line in enumerate(lines):
+    for x, char in enumerate(line):
+        if char == "S":
+            beam_starts.add((x, new_y))
+        if char == "^":
+            splitters.add((x, new_y))
 
-### This is all terrible
+splits = 0
+# while True:
+#     new_beam_starts = set()
+#     for x, y in beam_starts:
+#         for new_y in range(y + 1, len(lines)):
+#             if (x, new_y) in all_beams:
+#                 break
 
-results = []
-opers = list(re.finditer(r"\*|\+", lines[-1]))
-for o, curr in enumerate(opers):
-    if o < len(opers) - 1:
-        section = (curr.span()[0], opers[o + 1].span()[0] - 1)
-    else:
-        section = (curr.span()[0], len(lines[0]))
+#             if (x, new_y) in splitters:
+#                 is_split = False
+#                 for diff in [-1, 1]:
+#                     pos = (x + diff, new_y)
+#                     if pos in all_beams:
+#                         continue
 
-    nums = defaultdict(str)
-    for c in range(*section):
-        for line in lines[:-1]:
-            if line[c]:
-                nums[c] += line[c]
+#                     new_beam_starts.add(pos)
+#                     all_beams.add(pos)
+#                     is_split = True
 
-    p_nums = map(int, nums.values())
-    if lines[-1][curr.span()[0]] == "+":
-        results.append(sum(p_nums))
-    else:
-        results.append(prod(p_nums))
+#                 if is_split:
+#                     splits += 1
+#                 break
 
-print(sum(results))
+#             all_beams.add((x, new_y))
+
+#     if not new_beam_starts:
+#         break
+#     beam_starts = new_beam_starts
+
+# print(splits)
+
+beams: dict[tuple[int, int], int] = defaultdict(int)
+for beam in beam_starts:
+    beams[beam] = 0
+
+
+@cache
+def do(pos: tuple[int, int], splitters) -> int:
+    res = 0
+    x, y = pos
+
+    for new_y in range(y + 1, len(lines)):
+        if new_y == len(lines) - 1:
+            return 1
+
+        if (x, new_y) in splitters:
+            for diff in [-1, 1]:
+                new_pos = (x + diff, new_y)
+                res += do(new_pos, splitters)
+            break
+
+    return res
+
+
+c = frozenset(splitters)
+
+print(do(list(beam_starts)[0], c))
